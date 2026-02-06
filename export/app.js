c641dd84fc1bcc6e9aa39d5f12f9310669b0440b
Farzam Fatah <fxrzam@gmail.com>
2026-02-06 18:45:01 +0300

    first commit

---
+++ export/app.js
@@ -0,0 +1,10 @@
+const math = require ('./math')
+
+console.log(math.add(9,9))
+console.log(math.square(9))
+console.log(math.PI)
+
+
+const cats = require('../shelter')
+
+console.log(cats)
---
+++ export/math.js
@@ -0,0 +1,14 @@
+const add = (x,y) => x + y;
+
+const PI= 3.14159;
+
+const square = x => x * x;
+
+
+const math = {
+    add: add,
+    square: square,
+    PI: PI
+}
+
+module.exports = math;
